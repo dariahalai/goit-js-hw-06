a01f31b10fefbf64533c0a9d8c27ee123b2da455
Halai Daria <halaidaria@gmail.com>
2022-10-07 13:44:50 +0300

    markup

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+const listEl = document.querySelector('#categories');
+console.log(`Number of categories:`,listEl.children.length)    
+const items = listEl.children;
+for (const item of items) {
+console.log(`Category:`,item.firstElementChild.textContent);
+const itemListEl = item.lastElementChild.children;
+console.log(`Elements:`,itemListEl.length);   
+}
